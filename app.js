c153a5cd2034f84849f70d335cdbb63b79296ab0
Taha Muslu <taha-_muslu1216@hotmail.com>
2022-08-23 11:16:29 +0300

    ejs ile entegre edildi

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
-const port = 3000;
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//MIDDLEWARES
+app.use(express.static("public"));
+
 
 
 app.get('/', (req, res) => {
+    res.render("index")
+});
 
-    const blog = {
-        id: 1,
-        name: "CleanBlog",
-        description: "clean blog desc"
-    }
-    res.send(blog);
-})
+app.get('/about', (req, res) => {
+    res.render("about")
+});
 
+app.get('/add_post', (req, res) => {
+    res.render("add_post")
+});
+
+app.get('/post', (req, res) => {
+    res.render("post")
+});
+
+
+
+const port = 3000;
 
 
 app.listen(port, () => {
